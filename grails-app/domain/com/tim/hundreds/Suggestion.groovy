6b202e9cbbaa8b6d00bd6c4d978bc734562ca98d
josdem <joseluis.delacruz@gmail.com>
2015-05-22 15:25:59 -0500

    #9 Agregando telefono como entidad de dominio

--- grails-app/domain/com/tim/hundreds/Suggestion.groovy
+++ grails-app/domain/com/tim/hundreds/Suggestion.groovy
@@ -6,6 +6,14 @@ class Suggestion {
   String contactName
   String email
 
+  Telephone telephone
+
+  static hasOne = [ telephone : Telephone ]
+
   static constraints = {
+    name blank:false,size:1..100
+    contactName blank:false,size:1..100
+    email blank:false,email:true,unique:true,size:1..200
   }
+
 }
