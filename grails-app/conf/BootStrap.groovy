f9c2d8c52535c0e1424659966d7d098b31d7f379
josdem <joseluis.delacruz@gmail.com>
2015-05-26 16:16:39 -0500

    #1 Agregando a josdem comom usuario inicial

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
-class BootStrap {
+import com.tim.hundreds.Role
+import com.tim.hundreds.User
+import com.tim.hundreds.Profile
+import com.tim.hundreds.UserRole
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap {
+  def init = { servletContext ->
+    def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+    def user = new User(username: 'josdem', password: '12345678')
+    def profile = new Profile(email:'me@techminds.com.mx', firstName:'me', middleName:'middleName', lastName:'lastName')
+    user.profile = profile
+    user.save(flush: true)
+
+    UserRole.create user, userRole, true
+
+    assert User.count() == 1
+    assert Role.count() == 1
+    assert UserRole.count() == 1
+  }
+  def destroy = {
+  }
 }
