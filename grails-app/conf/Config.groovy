1196107b97e593552b3f0c097c68a0c268404992
neodevelop <neodevelop@gmail.com>
2015-06-06 16:17:51 -0500

    Ajustando la salida del log a un archivo

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -85,39 +85,51 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+def logFolder = 'target'
 environments {
     development {
         grails.logging.jul.usebridge = true
     }
     production {
         grails.logging.jul.usebridge = false
+        logFolder = System.getProperty('catalina.base') ?: 'logs'
         // TODO: grails.serverURL = "http://www.changeme.com"
     }
 }
 
 // log4j configuration
 log4j.main = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    appenders {
-        console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    }
 
-    info 'grails.app.controllers.com.tim.hundreds',
-         'grails.app.services.com.tim.hundreds',
-         'grails.app.domain.com.tim.hundreds'
+  appenders {
+    appender new org.apache.log4j.DailyRollingFileAppender(
+      name: "myAppender",
+      layout: pattern(conversionPattern: '%d{ISO8601}\t%p\t%c:%L\t%m%n'),
+      file: "${logFolder}/cienhundreds.log")
+  }
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+  root {
+    debug 'myAppender'
+  }
+
+  info 'grails.app.controllers.com.tim.hundreds',
+       'grails.app.services.com.tim.hundreds',
+       'grails.app.domain.com.tim.hundreds'
+
+  error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+         'org.codehaus.groovy.grails.web.pages',          // GSP
+         'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+         'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+         'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+         'org.codehaus.groovy.grails.commons',            // core / classloading
+         'org.codehaus.groovy.grails.plugins',            // plugins
+         'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+         'org.springframework',
+         'org.hibernate',
+         'net.sf.ehcache.hibernate',
+         'org.apache',
+         'org.apache.naming',
+         'grails.plugin',
+         'org.codehaus.groovy'
 }
 
 
