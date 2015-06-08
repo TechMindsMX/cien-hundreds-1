16a9e48d9690f5e954f203e96f9765997176387b
josdem <joseluis.delacruz@gmail.com>
2015-06-08 15:26:45 -0500

    #8 Agregando test de integracion  para validar que no se pueden ingresar mas de 3 sugerencias al musico

---
+++ test/integration/com/tim/hundreds/SuggestionIntegrationSpec.groovy
@@ -0,0 +1,64 @@
+package com.tim.hundreds
+
+import spock.lang.Specification
+import grails.validation.ValidationException
+
+/**
+ *
+ */
+class SuggestionIntegrationSpec extends Specification {
+
+  def suggestionService
+
+  void "Should save an musician with suggestion"() {
+    given: "An musician"
+      def musician = new Musician(name:'name',history:'history')
+      musician.genre = Genre.TRANCE
+      musician.hasManager = true
+      musician.dateCreated = new Date()
+      musician.lastUpdated = new Date()
+      musician.formed = new Date()
+    and: "We save musician"
+      musician.save(flush: true)
+    and: "We create a suggestion"
+      def suggestionInstance = new Suggestion(name:'name',contactName:'contactName',email:'josdem@email.com',phone:'1234567890')
+      suggestionInstance.musician = musician
+    when: "We save suggestion"
+      def result = suggestionService.save(suggestionInstance)
+    then:"We validate command"
+      result
+    cleanup:"We delete musician"
+      musician.delete()
+  }
+
+  void "Should not save an musician with more than 3 suggestions"() {
+    given: "An musician"
+      def musician = new Musician(name:'name',history:'history')
+      musician.genre = Genre.TRANCE
+      musician.hasManager = true
+      musician.dateCreated = new Date()
+      musician.lastUpdated = new Date()
+      musician.formed = new Date()
+    and: "We save musician"
+      musician.save(flush: true)
+    and: "We create an suggestion"
+      def suggestionInstance1 = new Suggestion(name:'name',contactName:'contactName',email:'josdem@email.com',phone:'1234567890')
+      suggestionInstance1.musician = musician
+      def suggestionInstance2 = new Suggestion(name:'name',contactName:'contactName',email:'josdem@email.com',phone:'1234567890')
+      suggestionInstance2.musician = musician
+      def suggestionInstance3 = new Suggestion(name:'name',contactName:'contactName',email:'josdem@email.com',phone:'1234567890')
+      suggestionInstance3.musician = musician
+      def suggestionInstance4 = new Suggestion(name:'name',contactName:'contactName',email:'josdem@email.com',phone:'1234567890')
+      suggestionInstance4.musician = musician
+    when: "We save suggestion"
+      suggestionService.save(suggestionInstance1)
+      suggestionService.save(suggestionInstance2)
+      suggestionService.save(suggestionInstance3)
+      suggestionService.save(suggestionInstance4)
+    then:"We expect exception"
+        thrown ValidationException
+    cleanup:"We delete musician"
+      musician.delete()
+  }
+
+}
