047ac202f3d28909f225f121dfc8019261ab0e56
josdem <joseluis.delacruz@gmail.com>
2015-06-08 14:30:00 -0500

    #8 Agregando test de integracion  para validar que no se pueden ingresar mas de 5 videos al musico

---
+++ test/integration/com/tim/hundreds/VideoIntegrationSpec.groovy
@@ -0,0 +1,70 @@
+package com.tim.hundreds
+
+import spock.lang.Specification
+import grails.validation.ValidationException
+
+/**
+ *
+ */
+class VideoIntegrationSpec extends Specification {
+
+  def videoService
+
+  void "Should save an musician with video"() {
+    given: "An musician"
+      def musician = new Musician(name:'name',history:'history')
+      musician.genre = Genre.TRANCE
+      musician.hasManager = true
+      musician.dateCreated = new Date()
+      musician.lastUpdated = new Date()
+      musician.formed = new Date()
+    and: "We save musician"
+      musician.save(flush: true)
+    and: "We create an Video"
+      def videoInstance = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance.musician = musician
+    when: "We save video"
+      def result = videoService.saveVideo(videoInstance)
+    then:"We validate command"
+      result
+    cleanup:"We delete musician"
+      musician.delete()
+  }
+
+  void "Should not save an musician with more than 5 videos"() {
+    given: "An musician"
+      def musician = new Musician(name:'name',history:'history')
+      musician.genre = Genre.TRANCE
+      musician.hasManager = true
+      musician.dateCreated = new Date()
+      musician.lastUpdated = new Date()
+      musician.formed = new Date()
+    and: "We save musician"
+      musician.save(flush: true)
+    and: "We create an Video"
+      def videoInstance1 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance1.musician = musician
+      def videoInstance2 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance2.musician = musician
+      def videoInstance3 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance3.musician = musician
+      def videoInstance4 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance4.musician = musician
+      def videoInstance5 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance5.musician = musician
+      def videoInstance6 = new Video(path:'https://www.youtube.com/watch?v=LKckM5gq7VU')
+      videoInstance6.musician = musician
+    when: "We save video"
+      videoService.saveVideo(videoInstance1)
+      videoService.saveVideo(videoInstance2)
+      videoService.saveVideo(videoInstance3)
+      videoService.saveVideo(videoInstance4)
+      videoService.saveVideo(videoInstance5)
+      videoService.saveVideo(videoInstance6)
+    then:"We expect exception"
+        thrown ValidationException
+    cleanup:"We delete musician"
+      musician.delete()
+  }
+
+}
