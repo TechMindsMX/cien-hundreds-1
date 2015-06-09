c1e965f887eac317585200a547a56cf03034904f
josdem <joseluis.delacruz@gmail.com>
2015-06-09 10:52:08 -0500

    #12 Agregando test para validar los datos de un complemento de producto

--- test/unit/com/tim/hundreds/ComplementSpec.groovy
+++ test/unit/com/tim/hundreds/ComplementSpec.groovy
@@ -2,6 +2,7 @@ package com.tim.hundreds
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,30 @@ import spock.lang.Specification
 @TestFor(Complement)
 class ComplementSpec extends Specification {
 
-    def setup() {
-    }
+  @Unroll
+  void """When we have a complement with name: #name and price: #price we expect result: #result"""() {
+  given: "A complement"
+    def complement = new Complement()
+  and: "A product"
+    def product = new Product()
+    complement.product = product
+  when: "We assign values"
+    complement.name = name
+    complement.price = price
+  then: "We validate result"
+    result == complement.validate()
+  where: "We have next cases"
+  name   | price || result
+  'name' | 100   || true
+  'n'    | 100   || true
+  'name' | 0.01  || true
+  ''     | 100   || false
+  null   | 100   || false
+  'name' | 0     || false
+  'name' | 0.00  || false
+  'name' | -0.01 || false
+  'name' | -1    || false
+  'name' | null  || false
+  }
 
-    def cleanup() {
-    }
-
-    void "test something"() {
-    }
 }
