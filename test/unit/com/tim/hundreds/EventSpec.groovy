139bba067e7f6f743c72695f34ee4af8b9cea789
josdem <joseluis.delacruz@gmail.com>
2015-06-09 12:50:02 -0500

    #124 Agregando test para validar los datos de los eventos

--- test/unit/com/tim/hundreds/EventSpec.groovy
+++ test/unit/com/tim/hundreds/EventSpec.groovy
@@ -2,6 +2,7 @@ package com.tim.hundreds
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,28 @@ import spock.lang.Specification
 @TestFor(Event)
 class EventSpec extends Specification {
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
-    }
+  @Unroll
+  void """When we have an event with activity: #activity, place: #place, date: #date we expect result is: #result"""() {
+  given: "A Event"
+    def event = new Event()
+  and: "A company"
+    def company = new Company()
+    event.company = company
+  when: "We assign values"
+    event.activity = activity
+    event.place = place
+    event.date = date
+  then: "We validate value"
+    result == event.validate()
+  where: "We have next values"
+  activity   | place   | date       || result
+  'activity' | 'place' | new Date() || true
+  'a'        | 'place' | new Date() || true
+  'activity' | 'p'     | new Date() || true
+  ''         | 'place' | new Date() || false
+  null       | 'place' | new Date() || false
+  'activity' | ''      | new Date() || false
+  'activity' | null    | new Date() || false
+  'activity' | 'place' | null       || false
+  }
 }
