8662ee4d0d80cb32152ada9f748b94cf2c2dd187
josdem <joseluis.delacruz@gmail.com>
2015-06-08 21:48:30 -0500

    #87 Agregando test de integracion que valida el numero de emails para un contacto

--- grails-app/services/com/tim/hundreds/EmailService.groovy
+++ grails-app/services/com/tim/hundreds/EmailService.groovy
@@ -6,7 +6,7 @@ import grails.transaction.Transactional
 class EmailService {
 
   def save(emailInstance, contact){
-    log.info "Emails count by contact: ${contact.emails.size()}"
+    log.info "Emails count by contact: ${contact.emails?.size()}"
 
     contact.addToEmails(emailInstance)
     contact.save(failOnError: true)
