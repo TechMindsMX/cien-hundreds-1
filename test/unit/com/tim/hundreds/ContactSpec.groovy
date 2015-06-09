de5b77a94003772d87ed9023a865bd177106ecbb
josdem <joseluis.delacruz@gmail.com>
2015-06-08 21:08:30 -0500

    #87 Agregando test de unidad que valida los datos de un contacto

--- test/unit/com/tim/hundreds/ContactSpec.groovy
+++ test/unit/com/tim/hundreds/ContactSpec.groovy
@@ -2,6 +2,7 @@ package com.tim.hundreds
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,57 @@ import spock.lang.Specification
 @TestFor(Contact)
 class ContactSpec extends Specification {
 
-    def setup() {
-    }
+  @Unroll
+  void """When we have an contact with firstName: #firstName, middleName: #middleName, lastName: #lastName, type: #type and role: #role and we validate, we expect result: #result"""() {
+  given: "An contact"
+    def contact = new ContactCommand()
+  when: "We assign values to command"
+    contact.firstName = firstName
+    contact.middleName = middleName
+    contact.lastName = lastName
+    contact.birthDate = birthDate
+    contact.entryDate = entryDate
+    contact.type = type
+    contact.role = role
+  then: "We validate command"
+    result == contact.validate()
+  where: "We have next cases"
+  firstName   | middleName   | lastName   | birthDate  | entryDate   | type              | role             || result
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || true
+  'f'         | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || true
+  'firstName' | 'm'          | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || true
+  'firstName' | 'middleName' | 'l'        | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || true
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.FEMALE | RoleType.MANAGER || true
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.ELEMENT || true
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.OTHER   || true
+  }
 
-    def cleanup() {
-    }
+  @Unroll
+  void """When we have an contact with firstName: #firstName, middleName: #middleName, lastName: #lastName, type: #type and role: #role and we validate, we expect result: #result"""() {
+  given: "An contact"
+    def contact = new ContactCommand()
+  when: "We assign values to command"
+    contact.firstName = firstName
+    contact.middleName = middleName
+    contact.lastName = lastName
+    contact.birthDate = birthDate
+    contact.entryDate = entryDate
+    contact.type = type
+    contact.role = role
+  then: "We validate command"
+    result == contact.validate()
+  where: "We have next cases"
+  firstName   | middleName   | lastName   | birthDate  | entryDate   | type              | role             || result
+  ''          | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  null        | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | ''           | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | null         | 'lastName' | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | 'middleName' | ''         | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | 'middleName' | null       | new Date() | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | 'middleName' | 'lastName' | null       | new Date()  | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | 'middleName' | 'lastName' | new Date() | null        | GenderType.MALE   | RoleType.MANAGER || false
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | null              | RoleType.MANAGER || false
+  'firstName' | 'middleName' | 'lastName' | new Date() | new Date()  | GenderType.MALE   | null             || false
+  }
 
-    void "test something"() {
-    }
 }
