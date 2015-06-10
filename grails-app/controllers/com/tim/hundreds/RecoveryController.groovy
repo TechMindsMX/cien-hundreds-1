132a2445e9fd5835973469ba13f2b7cd056247ee
josdem <joseluis.delacruz@gmail.com>
2015-06-10 12:02:54 -0500

    #5 Agregando el controller para recuperar el password

---
+++ grails-app/controllers/com/tim/hundreds/RecoveryController.groovy
@@ -0,0 +1,51 @@
+package com.tim.hundreds
+
+class RecoveryController {
+  def recoveryService
+
+  def index() { }
+
+  def show() {
+    try {
+      recoveryService.obtainRegistrationCodeForToken(params.id)
+      respond OK
+    }catch(BusinessException be) {
+      render status:NOT_FOUND
+    }
+  }
+
+  def save() {
+    def email = params.email
+    try {
+      recoveryService.generateRegistrationCodeForEmail(email)
+      flash.message = "Un mensaje se te ha enviado al correo"
+      redirect action:'index'
+    }catch(BusinessException be) {
+      flash.error = "Hubo un problema con el correo indicado"
+      redirect action:'index'
+    }
+  }
+
+  def update(ChangePasswordCommand command) {
+    if(command.hasErrors()) {
+      respond command.errors, view:'show', id:params.id
+      return
+    }
+    recoveryService.changePasswordForToken(params.id, command.password)
+    redirect controller:'login', action:'auth'
+  }
+
+}
+
+class ChangePasswordCommand {
+  String password
+  String confirmPassword
+
+  static constraints = {
+    password(blank:false, size:10..50, matches:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{10,}$/,validator:{val, obj ->
+      if(!val.equalsIgnoreCase(obj.confirmPassword)) {
+        return false
+      }
+    })
+  }
+}
