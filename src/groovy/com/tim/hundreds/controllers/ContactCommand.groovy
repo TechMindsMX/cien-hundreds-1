b00480b568f5b0575cbbdfa000246760dce4cc0d
josdem <joseluis.delacruz@gmail.com>
2015-06-17 11:20:23 -0500

    #125 Agregando test de integracion para validar los casos del tag

--- src/groovy/com/tim/hundreds/controllers/ContactCommand.groovy
+++ src/groovy/com/tim/hundreds/controllers/ContactCommand.groovy
@@ -31,7 +31,15 @@ class ContactCommand {
   ]
 
   static constraints = {
-    importFrom Contact
+    firstName blank:false,size:1..100
+    lastName blank:false,size:1..100
+    motherLastName blank:false,size:1..100
+    other nullable:true,blank:false,size:1..50
+    nationality nullable:true,blank:false,size:1..50
+    biography nullable:true,blank:false,size:1..1000
+    style nullable:true,blank:false,size:1..1000
+    social nullable:true
+    photoPath nullable:true
   }
 
 }
