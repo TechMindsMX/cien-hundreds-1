4c739c4f2015565d7428c720aec276b666d78f9d
josdem <joseluis.delacruz@gmail.com>
2015-06-24 13:20:45 -0500

    #27 Agregando las vistas y controllers del giro de actividad

---
+++ grails-app/controllers/com/tim/hundreds/BusinessActivityController.groovy
@@ -0,0 +1,106 @@
+package com.tim.hundreds
+
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_USER','ROLE_ADMIN'])
+@Transactional(readOnly = true)
+class BusinessActivityController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond BusinessActivity.list(params), model:[businessActivityInstanceCount: BusinessActivity.count()]
+    }
+
+    def show(BusinessActivity businessActivityInstance) {
+        respond businessActivityInstance
+    }
+
+    def create() {
+        respond new BusinessActivity(params)
+    }
+
+    @Transactional
+    def save(BusinessActivity businessActivityInstance) {
+        if (businessActivityInstance == null) {
+            notFound()
+            return
+        }
+
+        if (businessActivityInstance.hasErrors()) {
+            respond businessActivityInstance.errors, view:'create'
+            return
+        }
+
+        businessActivityInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'businessActivity.label', default: 'BusinessActivity'), businessActivityInstance.id])
+                redirect businessActivityInstance
+            }
+            '*' { respond businessActivityInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(BusinessActivity businessActivityInstance) {
+        respond businessActivityInstance
+    }
+
+    @Transactional
+    def update(BusinessActivity businessActivityInstance) {
+        if (businessActivityInstance == null) {
+            notFound()
+            return
+        }
+
+        if (businessActivityInstance.hasErrors()) {
+            respond businessActivityInstance.errors, view:'edit'
+            return
+        }
+
+        businessActivityInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'BusinessActivity.label', default: 'BusinessActivity'), businessActivityInstance.id])
+                redirect businessActivityInstance
+            }
+            '*'{ respond businessActivityInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(BusinessActivity businessActivityInstance) {
+
+        if (businessActivityInstance == null) {
+            notFound()
+            return
+        }
+
+        businessActivityInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'BusinessActivity.label', default: 'BusinessActivity'), businessActivityInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'businessActivity.label', default: 'BusinessActivity'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/views/businessActivity/show.gsp
@@ -0,0 +1,44 @@
+
+<%@ page import="com.tim.hundreds.BusinessActivity" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="layout" content="main">
+		<g:set var="entityName" value="${message(code: 'businessActivity.label', default: 'BusinessActivity')}" />
+		<title><g:message code="default.show.label" args="[entityName]" /></title>
+	</head>
+	<body>
+		<a href="#show-businessActivity" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
+		<div class="nav" role="navigation">
+			<ul class="nav nav-pills">
+				<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
+				<li><g:link class="list" action="index"><g:message code="default.list.label" args="[entityName]" /></g:link></li>
+				<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
+			</ul>
+		</div>
+		<div id="show-businessActivity" class="content scaffold-show" role="main">
+			<h1><g:message code="default.show.label" args="[entityName]" /></h1>
+			<g:if test="${flash.message}">
+			<div class="message" role="status">${flash.message}</div>
+			</g:if>
+			<ul class="property-list businessActivity">
+			
+				<g:if test="${businessActivityInstance?.name}">
+				<li class="fieldcontain">
+					<span id="name-label" class="${session.labelWidth} property-label"><g:message code="businessActivity.name.label" default="Name" /></span>
+					
+						<span class="property-value" aria-labelledby="name-label"><g:fieldValue bean="${businessActivityInstance}" field="name"/></span>
+					
+				</li>
+				</g:if>
+			
+			</ul>
+			<g:form url="[resource:businessActivityInstance, action:'delete']" method="DELETE">
+				<fieldset class="buttons">
+					<g:link class="btn btn-primary edit" action="edit" resource="${businessActivityInstance}"><g:message code="default.button.edit.label" default="Edit" /></g:link>
+					<g:actionSubmit class="btn btn-danger delete" action="delete" value="${message(code: 'default.button.delete.label', default: 'Delete')}" onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" />
+				</fieldset>
+			</g:form>
+		</div>
+	</body>
+</html>
