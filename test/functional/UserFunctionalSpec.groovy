e2a08435c8c562bb5902cd31b68f3ef8bd495cec
josdem <joseluis.delacruz@gmail.com>
2015-06-25 20:54:25 -0500

    Completando el test

--- test/functional/UserFunctionalSpec.groovy
+++ test/functional/UserFunctionalSpec.groovy
@@ -9,15 +9,15 @@ class UserFunctionalSpec extends GebReportingSpec {
   def "should create an account"() {
   given:"User page"
     to UserPage
-  when: "I am entering all values"
+  when: "I am entering user values"
     userForm.username = 'josdem'
     userForm.password = 'aA12345678'
     userForm.passwordCheck = 'aA12345678'
     userForm.firstName = 'Jose Luis'
     userForm.middleName = 'De la Cruz'
     userForm.lastName = 'Morales'
-    userForm.email = 'josdem@email.com'
-    userForm.emailCheck = 'josdem@email.com'
+    userForm.email = 'joseluis.delacruz@gmail.com'
+    userForm.emailCheck = 'joseluis.delacruz@gmail.com'
     $("#terms").click()
     $("#create").click()
   then: "I am being redirected to the homepage"
