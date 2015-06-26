0772f945a272f5917f20ab3492001a117ae12c6b
josdem <joseluis.delacruz@gmail.com>
2015-06-25 19:40:56 -0500

    Segundo test exitoso en geb

--- test/functional/LoginSpec.groovy
+++ test/functional/LoginSpec.groovy
@@ -6,7 +6,20 @@ import page.HomePage
 @Stepwise
 class LoginSpec extends GebReportingSpec {
 
-  def "admin login"() {
+  def "Invalid admin login"() {
+  given:"Login page"
+    to LoginPage
+  when: "I am entering invalid password"
+    loginForm.j_username = "admin"
+    loginForm.j_password = "87654321"
+    $("#btn-success").click()
+  then: "I am being redirected to the homepage"
+    at LoginPage
+    !loginForm.j_username
+    !loginForm.j_password
+  }
+
+  def "Admin login"() {
   given:"Login page"
     to LoginPage
   when: "I am entering invalid password"
