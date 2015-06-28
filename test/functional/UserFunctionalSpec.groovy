ab85d309d40aaf8b6d9a174d4c9ebc79a381529d
josdem <joseluis.delacruz@gmail.com>
2015-06-28 11:55:40 -0500

    Agregando el test que evita reincidencia del bug

--- test/functional/UserFunctionalSpec.groovy
+++ test/functional/UserFunctionalSpec.groovy
@@ -6,6 +6,23 @@ import page.HomePage
 @Stepwise
 class UserFunctionalSpec extends GebReportingSpec {
 
+  def "should not create an account since user did not click in terms"() {
+  given:"User page"
+    to UserPage
+  when: "I am entering user values"
+    userForm.username = 'josdem'
+    userForm.password = 'aA12345678'
+    userForm.passwordCheck = 'aA12345678'
+    userForm.firstName = 'Jose Luis'
+    userForm.middleName = 'De la Cruz'
+    userForm.lastName = 'Morales'
+    userForm.email = 'joseluis.delacruz@gmail.com'
+    userForm.emailCheck = 'joseluis.delacruz@gmail.com'
+    $("#create").click()
+  then: "I am being redirected to the homepage"
+    at UserPage
+  }
+
   def "should create an account"() {
   given:"User page"
     to UserPage
