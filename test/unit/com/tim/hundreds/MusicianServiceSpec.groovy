00ea8e9855ba4fabfe8f1f7fa2cf553c24dddf5a
josdem <joseluis.delacruz@gmail.com>
2015-07-02 08:33:31 -0500

    #94 Agregando el test para asignar el facilitador al musico

--- test/unit/com/tim/hundreds/MusicianServiceSpec.groovy
+++ test/unit/com/tim/hundreds/MusicianServiceSpec.groovy
@@ -8,16 +8,20 @@ import spock.lang.Specification
  */
 @TestFor(MusicianService)
 class MusicianServiceSpec extends Specification {
-  def springSecurityService = Mock(SpringSecurityService)
 
-  def setup() {
-    service.springSecurityService = springSecurityService
-  }
-
-  void "should save an musician"() {
-  given: "An user"
-   def user = new User()
-  and: "And musician"
-   def musician = new Musician()
+  void "should assign musician to facilitator"() {
+  given: "An musician validation"
+   def musicianValidationInstance = Mock(MusicianValidation)
+  and: "And musician and user"
+   def musician = Mock(Musician)
+   def user = Mock(User)
+   musicianValidationInstance.musician >> musician
+   musicianValidationInstance.user >> user
+   when: "We assign values to the user"
+   service.assignMusicianToFacilitator(musicianValidationInstance)
+   then: "We expect musician is save"
+   1 * musician.setProperty('assigned',user)
+   1 * musician.save()
+   1 * musicianValidationInstance.save()
   }
 }
