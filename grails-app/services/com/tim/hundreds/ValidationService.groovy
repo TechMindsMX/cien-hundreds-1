f008f6593700e79fd9bedcbe11e7719f50ec500c
josdem <joseluis.delacruz@gmail.com>
2015-07-02 10:37:02 -0500

    #81 Agregando el test para aceptar o rechazar a un facilitador y enviar el correo

--- grails-app/services/com/tim/hundreds/ValidationService.groovy
+++ grails-app/services/com/tim/hundreds/ValidationService.groovy
@@ -4,15 +4,18 @@ import grails.transaction.Transactional
 
 @Transactional
 class ValidationService {
+  def restService
+  def grailsApplication
 
   def validate(musicianValidationInstance) {
     def musician = musicianValidationInstance.musician
+    def user = musicianValidationInstance.user
     def active = musicianValidationInstance.type == ValidationType.ACCEPTED ? true : false
     musician.active = active
     if(active){
-
+      def message = new FacilitatorCommand(email:user.profile.email, facilitator:"${user.profile.firstName} ${user.profile.middleName} ${user.profile.lastName}", musician:musician.name)
+      restService.sendCommand(message, grailsApplication.config.facilitator.assigned.url)
     }
-
   }
 
 }
--- test/unit/com/tim/hundreds/ValidationServiceSpec.groovy
+++ test/unit/com/tim/hundreds/ValidationServiceSpec.groovy
@@ -8,13 +8,48 @@ import spock.lang.Specification
  */
 @TestFor(ValidationService)
 class ValidationServiceSpec extends Specification {
+  def restService = Mock(RestService)
 
-    def setup() {
-    }
+  def setup() {
+    service.restService = restService
+    grailsApplication.config.facilitator.assigned.url = 'facilitatorAssignedUrl'
+  }
 
-    def cleanup() {
-    }
+  void "should validate an musician"() {
+  given: "An musician validation"
+    def musicianValidation = Mock(MusicianValidation)
+  and: "Musician, user and profile"
+    def user = Mock(User)
+    def musician = Mock(Musician)
+    def profile = Mock(Profile)
+  when: "We validate musician"
+    musicianValidation.type >> ValidationType.ACCEPTED
+    musicianValidation.user >> user
+    musicianValidation.musician >> musician
+    user.profile >> profile
+    service.validate(musicianValidation)
+  then: "We expect musician validated"
+    1 * musician.setProperty('active', true)
+    1 * restService.sendCommand(_ as FacilitatorCommand, 'facilitatorAssignedUrl')
+  }
 
-    void "test something"() {
-    }
+  void "should refuse an musician"() {
+  given: "An musician validation"
+    def musicianValidation = Mock(MusicianValidation)
+  and: "Musician, user and profile"
+    def user = Mock(User)
+    def musician = Mock(Musician)
+    def profile = Mock(Profile)
+  when: "We validate musician"
+    musicianValidation.type >> ValidationType.REFUSED
+    musicianValidation.user >> user
+    musicianValidation.musician >> musician
+    user.profile >> profile
+    service.validate(musicianValidation)
+  then: "We expect musician validated"
+    1 * musician.setProperty('active', false)
+    0 * restService.sendCommand(_ as FacilitatorCommand, 'facilitatorAssignedUrl')
+  }
+
+
 }
