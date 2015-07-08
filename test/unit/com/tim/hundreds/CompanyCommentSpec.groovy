534a5155701fec97bfedb5c46665177c3587ec85
josdem <joseluis.delacruz@gmail.com>
2015-07-08 12:34:07 -0500

    #76 Completando las combinaciones de validacion para los comentarios de la empresa

--- test/unit/com/tim/hundreds/CompanyCommentSpec.groovy
+++ test/unit/com/tim/hundreds/CompanyCommentSpec.groovy
@@ -27,5 +27,26 @@ class CompanyCommentSpec extends Specification {
   where:"We have next values"
     general   | description   | datosFiscales   | events   | notes   | references   | collaborators   || result
     'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'g'       | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'd'           | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | 'd'             | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'e'      | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | 'n'     | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | 'r'          | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | 'c'             || true
+    null      | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | null          | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | null            | 'events' | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | null     | 'notes' | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | null    | 'references' | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | null         | 'collaborators' || true
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | null            || true
+    ''        | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || false
+    'general' | ''            | 'datosFiscales' | 'events' | 'notes' | 'references' | 'collaborators' || false
+    'general' | 'description' | ''              | 'events' | 'notes' | 'references' | 'collaborators' || false
+    'general' | 'description' | 'datosFiscales' | ''       | 'notes' | 'references' | 'collaborators' || false
+    'general' | 'description' | 'datosFiscales' | 'events' | ''      | 'references' | 'collaborators' || false
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | ''           | 'collaborators' || false
+    'general' | 'description' | 'datosFiscales' | 'events' | 'notes' | 'references' | ''              || false
   }
 }
