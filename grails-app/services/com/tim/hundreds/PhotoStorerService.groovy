45992d73d430060d3df52b8b0999eed68c91daa9
josdem <joseluis.delacruz@gmail.com>
2015-07-08 17:00:42 -0500

    Agregando el token generator en los storers

--- grails-app/services/com/tim/hundreds/PhotoStorerService.groovy
+++ grails-app/services/com/tim/hundreds/PhotoStorerService.groovy
@@ -4,12 +4,11 @@ import grails.transaction.Transactional
 
 @Transactional
 class PhotoStorerService implements StorerService {
-  def tokenService
   def photoPath = ApplicationState.ATTACH_DIR + ApplicationState.PHOTO_DIRECTORY
   def directoryDestination = new File(photoPath)
 
   String storeFile(def multipartFile){
-    def fileName = tokenService.generateToken() + ApplicationState.IMAGE_EXTENSION
+    def fileName = TokenGenerator.generateToken() + ApplicationState.IMAGE_EXTENSION
     File fileDestination = new File(directoryDestination,fileName)
     multipartFile.transferTo(fileDestination)
     fileName
--- grails-app/services/com/tim/hundreds/ResumeStorerService.groovy
+++ grails-app/services/com/tim/hundreds/ResumeStorerService.groovy
@@ -4,12 +4,11 @@ import grails.transaction.Transactional
 
 @Transactional
 class ResumeStorerService implements StorerService {
-  def tokenService
   def resumePath = ApplicationState.ATTACH_DIR + ApplicationState.RESUME_DIRECTORY
   def directoryDestination = new File(resumePath)
 
   String storeFile(def multipartFile){
-    def fileName = tokenService.generateToken() + ApplicationState.RESUME_EXTENSION
+    def fileName = TokenGenerator.generateToken() + ApplicationState.RESUME_EXTENSION
     File fileDestination = new File(directoryDestination,fileName)
     multipartFile.transferTo(fileDestination)
     fileName
