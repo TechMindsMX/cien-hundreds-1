28862e19bfe0e641653373151d624139c087a92e
josdem <joseluis.delacruz@gmail.com>
2015-07-09 09:52:21 -0500

    modificando el bootstrap para creacion de catalogos

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,73 +10,68 @@ import grails.util.Environment
 
 class BootStrap {
   def init = { servletContext ->
-  if(Environment.current == Environment.DEVELOPMENT || Environment.current == Environment.TEST) {
-      createAllRole()
-      createUserWithRole('ROLE_USER',             'cien',           'me@techminds.com.mx')
-      createUserWithRole('ROLE_ADMIN',            'admin',          'administrator@trama.mx')
-      createUserWithRole('ROLE_FACILITATOR',      'facilitator',    'usuario1@trama.mx')
-      createUserWithRole('ROLE_BUYER',            'buyer',          'usuario2@trama.mx')
-      createUserWithRole('ROLE_MUSICIAN_ADMIN',   'musicicanAdmin', 'QA@trama.mx')
-      createUserWithRole('ROLE_COMPANY_ADMIN',    'companyAdmin',   'remy.ochoa@trama.mx')
-      createUserWithRole('ROLE_MUSICIAN_VIEWER',  'musicianViewer', 'aleister.r@gmail.com')
-      createUserWithRole('ROLE_COMPANY_VIEWER',   'companyViewer',  'demonio.r@gmail.com')
-      createGenres()
-      createMusicianRoles()
-      createBusinessActivity()
-    }
+    createAllRole()
+    createUserWithRole('ROLE_USER',             'cien',           'me@techminds.com.mx')
+    createUserWithRole('ROLE_ADMIN',            'admin',          'administrator@trama.mx')
+    createUserWithRole('ROLE_FACILITATOR',      'facilitator',    'usuario1@trama.mx')
+    createUserWithRole('ROLE_BUYER',            'buyer',          'usuario2@trama.mx')
+    createUserWithRole('ROLE_MUSICIAN_ADMIN',   'musicicanAdmin', 'QA@trama.mx')
+    createUserWithRole('ROLE_COMPANY_ADMIN',    'companyAdmin',   'remy.ochoa@trama.mx')
+    createUserWithRole('ROLE_MUSICIAN_VIEWER',  'musicianViewer', 'aleister.r@gmail.com')
+    createUserWithRole('ROLE_COMPANY_VIEWER',   'companyViewer',  'demonio.r@gmail.com')
+    createGenres()
+    createMusicianRoles()
+    createBusinessActivity()
   }
 
   def destroy = {
   }
 
   def createAllRole(){
-    new Role(authority: 'ROLE_USER').save(flush: true)
-    new Role(authority: 'ROLE_ADMIN').save(flush: true)
-    new Role(authority: 'ROLE_FACILITATOR').save(flush: true)
-    new Role(authority: 'ROLE_BUYER').save(flush: true)
-    new Role(authority: 'ROLE_MUSICIAN_ADMIN').save(flush: true)
-    new Role(authority: 'ROLE_COMPANY_ADMIN').save(flush: true)
-    new Role(authority: 'ROLE_MUSICIAN_VIEWER').save(flush: true)
-    new Role(authority: 'ROLE_COMPANY_VIEWER').save(flush: true)
-    assert Role.count() == 8
+    new Role.findOrCreateWhere(authority: 'ROLE_USER')
+    new Role.findOrCreateWhere(authority: 'ROLE_ADMIN')
+    new Role.findOrCreateWhere(authority: 'ROLE_FACILITATOR')
+    new Role.findOrCreateWhere(authority: 'ROLE_BUYER')
+    new Role.findOrCreateWhere(authority: 'ROLE_MUSICIAN_ADMIN')
+    new Role.findOrCreateWhere(authority: 'ROLE_COMPANY_ADMIN')
+    new Role.findOrCreateWhere(authority: 'ROLE_MUSICIAN_VIEWER')
+    new Role.findOrCreateWhere(authority: 'ROLE_COMPANY_VIEWER')
   }
 
-  def createUserWithRole(String authority,String name,String email) {
-    def userCount = User.count()
-
+  def createUserWithRole(String authority, String name, String email) {
     def userRole = Role.findByAuthority(authority)
-    def user = new User(username: name, password: '12345678')
-    def  profile = new Profile(email: email, firstName: name, middleName:'middleName', lastName:'lastName').save()
-    user.profile = profile
-    user.enabled = true
-    user.save(flush: true)
+    def user = new User(username: name, password: 'T3chminds2015')
 
-    UserRole.create user, userRole, true
-
-    assert User.count() == (userCount + 1)
+    if(!user){
+      def  profile = new Profile(email: email, firstName: name, middleName:'middleName', lastName:'lastName').save()
+      user.profile = profile
+      user.enabled = true
+      user.save(flush: true)
+      UserRole.create user, userRole, true
+    }
   }
 
   def createGenres(){
-    new Genre(name:'Ambient').save()
-    new Genre(name:'Ballad').save()
-    new Genre(name:'Blues').save()
-    new Genre(name:'Jazz').save()
-    new Genre(name:'Latin').save()
-    new Genre(name:'Groove').save()
-    new Genre(name:'Progressive').save()
-    new Genre(name:'Pop').save()
-    new Genre(name:'Trance').save()
+    new Genre.findOrCreateWhere(name:'Ambient')
+    new Genre.findOrCreateWhere(name:'Ballad')
+    new Genre.findOrCreateWhere(name:'Blues')
+    new Genre.findOrCreateWhere(name:'Jazz')
+    new Genre.findOrCreateWhere(name:'Latin')
+    new Genre.findOrCreateWhere(name:'Groove')
+    new Genre.findOrCreateWhere(name:'Progressive')
+    new Genre.findOrCreateWhere(name:'Pop')
+    new Genre.findOrCreateWhere(name:'Trance')
   }
 
   def createMusicianRoles(){
-    new MusicianRole(name:'Manager').save()
-    new MusicianRole(name:'Element').save()
-    new MusicianRole(name:'Other').save()
+    new MusicianRole.findOrCreateWhere(name:'Manager').save()
+    new MusicianRole.findOrCreateWhere(name:'Element').save()
+    new MusicianRole.findOrCreateWhere(name:'Other').save()
   }
 
   def createBusinessActivity(){
-    new BusinessActivity(name:'Products').save()
-    new BusinessActivity(name:'Services').save()
+    new BusinessActivity.findOrCreateWhere(name:'Products')
+    new BusinessActivity.findOrCreateWhere(name:'Services')
   }
 
 }
