bb225fe07acc7e9bd8feb27176e71e1bc977a7cd
josdem <joseluis.delacruz@gmail.com>
2015-07-22 09:20:36 -0500

    #141 Agreagando los test que respaldan la funcionalidad de filtrar empresas por fecha de creacion

--- test/unit/com/tim/hundreds/CompanyServiceSpec.groovy
+++ test/unit/com/tim/hundreds/CompanyServiceSpec.groovy
@@ -3,18 +3,50 @@ package com.tim.hundreds
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(CompanyService)
+@Mock([Company])
 class CompanyServiceSpec extends Specification {
 
-    def setup() {
-    }
+  void "should get companies by creation date"() {
+  given: "A date range"
+    def endDate = new Date()
+    def startDate = endDate - 9
+  and: "A company"
+    def company = new Company()
+    company.dateCreated = endDate - 1
+    company.save(validate: false)
+  when: "We try to get company by range"
+    def result = service.getCompaniesByDateCreated(startDate, endDate)
+  then:
+    1 == result.size()
+  }
 
-    def cleanup() {
-    }
+  void "should not get companies by creation date since is not in range"() {
+  given: "A date range"
+    def endDate = new Date()
+    def startDate = endDate - 9
+  and: "A company"
+    def company = new Company()
+    company.dateCreated = endDate - 15
+    company.save(validate: false)
+  when: "We try to get company by range"
+    def result = service.getCompaniesByDateCreated(startDate, endDate)
+  then:
+    result.isEmpty()
+  }
 
-    void "test something"() {
-    }
+  void "should get companies created same day"() {
+  given: "A date range"
+    def endDate = new Date()
+    def startDate = endDate - 9
+  and: "A company"
+    def company = new Company()
+    company.dateCreated = endDate
+    company.save(validate: false)
+  when: "We try to get company by range"
+    def result = service.getCompaniesByDateCreated(startDate, endDate)
+  then:
+    1 == result.size()
+  }
+
 }
