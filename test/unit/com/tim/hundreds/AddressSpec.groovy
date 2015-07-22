397ec6871a39efbd9817101a97de575ed21a5fc1
Ricardo <ricardolyon@gmail.com>
2015-07-22 10:32:13 -0500

    AddressSpec actualizado

--- test/unit/com/tim/hundreds/AddressSpec.groovy
+++ test/unit/com/tim/hundreds/AddressSpec.groovy
@@ -22,29 +22,37 @@ class AddressSpec extends Specification {
     address.town = town
     address.state = state
     address.country = country
+
+    address.musician = musician
+    address.company = company
+    address.contact = contact
+    address.datosFiscales = datosFiscales
   then: "We validate address values"
     result == address.validate()
   where: "We have next cases"
-  street   | zipcode   | neighborhood   | county   | town   | state   | country   || result
-  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" || true
-  "street" | "12345"   | "neighborhood" | ''       | "town" | "state" | "country" || true
-  "street" | "12345"   | "neighborhood" | null     | "town" | "state" | "country" || true
-  ""       | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  null     | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "1234"    | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "123456"  | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "123"     | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "12"      | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "1"       | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | ""        | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | null      | "neighborhood" | 'county' | "town" | "state" | "country" || false
-  "street" | "12345"   | ""             | 'county' | "town" | "state" | "country" || false
-  "street" | "12345"   | null           | 'county' | "town" | "state" | "country" || false
-  "street" | "12345"   | "neighborhood" | 'county' | ""     | "state" | "country" || false
-  "street" | "12345"   | "neighborhood" | 'county' | null   | "state" | "country" || false
-  "street" | "12345"   | "neighborhood" | 'county' | "town" | ""      | "country" || false
-  "street" | "12345"   | "neighborhood" | 'county' | "town" | null    | "country" || false
-  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | ""        || false
-  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | null      || false
+  street   | zipcode   | neighborhood   | county   | town   | state   | country   | contact       | company       | datosFiscales       | musician       || result
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || true
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" | null          | new Company() | null                | null           || true
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" | null          | null          | new DatosFiscales() | null           || true
+  "street" | "12345"   | "neighborhood" | ''       | "town" | "state" | "country" | null          | null          | null                | new Musician() || true
+  "street" | "12345"   | "neighborhood" | ''       | "town" | "state" | "country" | new Contact() | null          | null                | null           || true
+  "street" | "12345"   | "neighborhood" | null     | "town" | "state" | "country" | new Contact() | null          | null                | null           || true
+  ""       | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  null     | "12345"   | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "1234"    | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "123456"  | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "123"     | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12"      | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "1"       | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | ""        | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | null      | "neighborhood" | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | ""             | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | null           | 'county' | "town" | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | ""     | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | null   | "state" | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | ""      | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | null    | "country" | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | ""        | new Contact() | null          | null                | null           || false
+  "street" | "12345"   | "neighborhood" | 'county' | "town" | "state" | null      | new Contact() | null          | null                | null           || false
   }
 }
