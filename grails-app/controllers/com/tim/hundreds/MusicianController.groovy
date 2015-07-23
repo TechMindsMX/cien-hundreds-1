b7424e04426fd7f684f8cdfa5fb3f54f954b3014
Ricardo <ricardolyon@gmail.com>
2015-07-23 10:38:06 -0500

    llistados de Musician solo para Owner y facilitator assigned

--- grails-app/controllers/com/tim/hundreds/MusicianController.groovy
+++ grails-app/controllers/com/tim/hundreds/MusicianController.groovy
@@ -18,8 +18,17 @@ class MusicianController {
 
     @Secured(['ROLE_USER','ROLE_ADMIN','ROLE_FACILITATOR','ROLE_MUSICIAN_ADMIN','ROLE_MUSICIAN_VIEWER'])
     def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Musician.list(params), model:[musicianInstanceCount: Musician.count()]
+        params.max = Math.min(max ?: 3, 100)
+
+        if (SpringSecurityUtils.ifAnyGranted('ROLE_USER')) {
+            def musicianList = Musician.findAllByUser(springSecurityService.currentUser, [max: params.max, sort: "name", order: "desc", offset: params.offset ?: 0])
+            respond musicianList, model:[musicianInstanceCount: musicianList.size()]
+        } else if (SpringSecurityUtils.ifAnyGranted('ROLE_FACILITATOR')) {
+            def musicianList = Musician.findAllByAssigned(springSecurityService.currentUser, [max: params.max, sort: "name", order: "desc", offset: params.offset ?: 0])
+            respond musicianList, model:[musicianInstanceCount: musicianList.size()]
+        } else {
+            respond Musician.list(params), model:[musicianInstanceCount: Musician.count()]
+        }
     }
 
     @Secured(['ROLE_USER','ROLE_ADMIN','ROLE_FACILITATOR','ROLE_MUSICIAN_ADMIN','ROLE_MUSICIAN_VIEWER'])
