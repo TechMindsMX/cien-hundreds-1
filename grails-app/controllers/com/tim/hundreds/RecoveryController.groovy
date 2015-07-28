d7418dd8d05076ff8e8fe35cdf1c7a25ba7bd0ca
josdem <joseluis.delacruz@gmail.com>
2015-07-28 15:11:22 -0500

    #221 Anexando la los mensajes de nuevas exceptions

--- grails-app/controllers/com/tim/hundreds/RecoveryController.groovy
+++ grails-app/controllers/com/tim/hundreds/RecoveryController.groovy
@@ -27,10 +27,14 @@ class RecoveryController {
       recoveryService.confirmAccountForToken(params.token)
       flash.message = g.message(code: 'login.confirm')
       redirect controller:'login', action:'auth'
-    }catch(BusinessException be) {
+    }catch(UserNotFoundException be) {
       flash.error = g.message(code: 'login.notfound')
       render status:NOT_FOUND
+    }catch(AccountNoEnabledException be) {
+      flash.error = g.message(code: 'login.account.disabled')
+      render status:NOT_FOUND
     }
+
   }
 
   def recoveryUser(){
