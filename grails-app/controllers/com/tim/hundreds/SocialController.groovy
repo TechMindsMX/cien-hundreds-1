bc4c8f09ec184253b7e097ebf1cd10f01a2fee99
ricardolyon <ricardolyon@gmail.com>
2015-08-01 09:15:52 -0500

    uuids en socialController findByUuid en show y edit, cambio en redirects de save y update

--- grails-app/controllers/com/tim/hundreds/SocialController.groovy
+++ grails-app/controllers/com/tim/hundreds/SocialController.groovy
@@ -8,113 +8,117 @@ import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_USER'])
 class SocialController {
-    def musicianService
-    def socialContextService
+  def musicianService
+  def socialContextService
 
-    static showMe = false /*Parametro para aparecer en el menú*/
+  static showMe = false /*Parametro para aparecer en el menú*/
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+  static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Social.list(params), model:[socialInstanceCount: Social.count()]
+  def index(Integer max) {
+    params.max = Math.min(max ?: 10, 100)
+    respond Social.list(params), model:[socialInstanceCount: Social.count()]
+  }
+
+  @Secured(['IS_AUTHENTICATED_REMEMBERED'])
+  def show(Social socialInstance) {
+    socialInstance = Social.findByUuid(params.uuid)
+    respond socialInstance
+  }
+
+  def create() {
+    def socialInstance = new Social(params)
+    socialInstance = setSocialParent(socialInstance, params)
+    respond social
+  }
+
+  @Transactional
+  def save(Social socialInstance) {
+    socialInstance = setSocialParent(socialInstance, params)
+    if (socialInstance == null) {
+      notFound()
+      return
     }
 
-    @Secured(['IS_AUTHENTICATED_REMEMBERED'])
-    def show(Social socialInstance) {
-        respond socialInstance
+    if (socialInstance.hasErrors()) {
+      respond socialInstance.errors, view:'create'
+      return
     }
 
-    def create() {
-      def social = new Social(params)
-      social.musician = Musician.findByUuid(params.musicianUuid)
-      social.company = Company.findByUuid(params.companyUuid)
-      social.contact = Contact.findByUuid(params.contactUuid)
-      respond social
+    socialContextService.saveInstance(socialInstance)
+
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.created.message', args: [message(code: 'social.label', default: 'Social'), socialInstance.id])
+        redirect action: 'show', params: [uuid: socialInstance.uuid]
+      }
+      '*' { respond socialInstance, [status: CREATED] }
+    }
+  }
+
+  def edit(Social socialInstance) {
+    socialInstance = Social.findByUuid(params.uuid)
+    socialInstance = setSocialParent(socialInstance, params)
+    respond socialInstance
+  }
+
+  @Transactional
+  def update(Social socialInstance) {
+    if (socialInstance == null) {
+      notFound()
+      return
     }
 
-    @Transactional
-    def save(Social socialInstance) {
-        socialInstance.musician = Musician.findByUuid(params.musicianUuid)
-        socialInstance.company = Company.findByUuid(params.companyUuid)
-        socialInstance.contact = Contact.findByUuid(params.contactUuid)
-        if (socialInstance == null) {
-            notFound()
-            return
-        }
-
-        if (socialInstance.hasErrors()) {
-            respond socialInstance.errors, view:'create'
-            return
-        }
-
-        socialContextService.saveInstance(socialInstance)
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'social.label', default: 'Social'), socialInstance.id])
-                redirect socialInstance
-            }
-            '*' { respond socialInstance, [status: CREATED] }
-        }
+    if (socialInstance.hasErrors()) {
+      respond socialInstance.errors, view:'edit'
+      return
     }
 
-    def edit(Social socialInstance) {
-      socialInstance.musician = Musician.findByUuid(params.musicianUuid)
-      socialInstance.company = Company.findByUuid(params.companyUuid)
-      socialInstance.contact = Contact.findByUuid(params.contactUuid)
-      respond socialInstance
+    socialInstance.save flush:true
+
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'Social.label', default: 'Social'), socialInstance.id])
+        redirect action: 'show', params: [uuid: socialInstance.uuid]
+      }
+      '*'{ respond socialInstance, [status: OK] }
+    }
+  }
+
+  @Transactional
+  def delete(Social socialInstance) {
+
+    if (socialInstance == null) {
+      notFound()
+      return
     }
 
-    @Transactional
-    def update(Social socialInstance) {
-        if (socialInstance == null) {
-            notFound()
-            return
-        }
+    socialContextService.deleteInstance(socialInstance)
 
-        if (socialInstance.hasErrors()) {
-            respond socialInstance.errors, view:'edit'
-            return
-        }
-
-        socialInstance.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Social.label', default: 'Social'), socialInstance.id])
-                redirect socialInstance
-            }
-            '*'{ respond socialInstance, [status: OK] }
-        }
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.deleted.message', args: [message(code: 'Social.label', default: 'Social'), socialInstance.id])
+        redirect action:"index", method:"GET"
+      }
+      '*'{ render status: NO_CONTENT }
     }
+  }
 
-    @Transactional
-    def delete(Social socialInstance) {
-
-        if (socialInstance == null) {
-            notFound()
-            return
-        }
-
-        socialContextService.deleteInstance(socialInstance)
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Social.label', default: 'Social'), socialInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
+  protected void notFound() {
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.not.found.message', args: [message(code: 'social.label', default: 'Social'), params.id])
+        redirect action: "index", method: "GET"
+      }
+      '*'{ render status: NOT_FOUND }
     }
+  }
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'social.label', default: 'Social'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+  private setSocialParent(Social socialInstance, params) {
+    if (params.musicianUuid) { socialInstance.musician = Musician.findByUuid(params.musicianUuid) }
+    else if (params.companyUuid) { socialInstance.company = Company.findByUuid(params.companyUuid) }
+    else if (params.contactUuid) { socialInstance.contact = Contact.findByUuid(params.contactUuid) }
+
+    socialInstance
+  }
 }
