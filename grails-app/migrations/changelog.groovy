5cccac8f098e2f27411458f244642e949f3c7a35
ricardolyon <ricardolyon@gmail.com>
2015-08-02 08:10:57 -0500

    Reference y Address, y refactor AddressController y SocialController

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1773,4 +1773,6 @@ databaseChangeLog = {
 	changeSet(author: "josdem (generated)", id: "1437493212037-96") {
 		addForeignKeyConstraint(baseColumnNames: "musician_id", baseTableName: "video", constraintName: "FK_s0eh4ylgywtxpqxhjk73yf0e6", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "musician", referencesUniqueColumn: "false")
 	}
+
+	include file: 'chnagelog-model-dates.groovy'
 }
---
+++ grails-app/migrations/chnagelog-model-dates.groovy
@@ -0,0 +1,434 @@
+databaseChangeLog = {
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-1") {
+		addColumn(tableName: "activity") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-2") {
+		addColumn(tableName: "activity") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-3") {
+		addColumn(tableName: "address") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-4") {
+		addColumn(tableName: "address") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-5") {
+		addColumn(tableName: "audio") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-6") {
+		addColumn(tableName: "audio") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-7") {
+		addColumn(tableName: "business_activity") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-8") {
+		addColumn(tableName: "business_activity") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-9") {
+		addColumn(tableName: "collaborator") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-10") {
+		addColumn(tableName: "collaborator") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-11") {
+		addColumn(tableName: "company_comment") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-12") {
+		addColumn(tableName: "company_comment") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-13") {
+		addColumn(tableName: "company_validation") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-14") {
+		addColumn(tableName: "company_validation") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-15") {
+		addColumn(tableName: "complement") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-16") {
+		addColumn(tableName: "complement") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-17") {
+		addColumn(tableName: "contact") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-18") {
+		addColumn(tableName: "contact") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-19") {
+		addColumn(tableName: "datos_fiscales") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-20") {
+		addColumn(tableName: "datos_fiscales") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-21") {
+		addColumn(tableName: "email") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-22") {
+		addColumn(tableName: "email") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-23") {
+		addColumn(tableName: "event") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-24") {
+		addColumn(tableName: "event") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-25") {
+		addColumn(tableName: "genre") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-26") {
+		addColumn(tableName: "genre") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-27") {
+		addColumn(tableName: "musician_comment") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-28") {
+		addColumn(tableName: "musician_comment") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-29") {
+		addColumn(tableName: "musician_role") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-30") {
+		addColumn(tableName: "musician_role") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-31") {
+		addColumn(tableName: "musician_validation") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-32") {
+		addColumn(tableName: "musician_validation") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-33") {
+		addColumn(tableName: "photo") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-34") {
+		addColumn(tableName: "photo") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-35") {
+		addColumn(tableName: "product") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-36") {
+		addColumn(tableName: "product") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-37") {
+		addColumn(tableName: "product_comment") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-38") {
+		addColumn(tableName: "product_comment") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-39") {
+		addColumn(tableName: "profile") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-40") {
+		addColumn(tableName: "profile") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-41") {
+		addColumn(tableName: "reference") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-42") {
+		addColumn(tableName: "reference") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-43") {
+		addColumn(tableName: "social") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-44") {
+		addColumn(tableName: "social") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-45") {
+		addColumn(tableName: "suggestion") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-46") {
+		addColumn(tableName: "suggestion") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-47") {
+		addColumn(tableName: "telephone") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-48") {
+		addColumn(tableName: "telephone") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-49") {
+		addColumn(tableName: "user") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-50") {
+		addColumn(tableName: "user") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-51") {
+		addColumn(tableName: "validation_message") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-52") {
+		addColumn(tableName: "validation_message") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-53") {
+		addColumn(tableName: "video") {
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "rlyon (generated)", id: "1438469931398-54") {
+		addColumn(tableName: "video") {
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+}
