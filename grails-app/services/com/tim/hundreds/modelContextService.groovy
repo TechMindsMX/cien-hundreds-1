c73a6ab81db072d1dfbb1863734234efcfee5d35
Ricardo <ricardolyon@gmail.com>
2015-08-03 11:18:39 -0500

    extrae metodos de SocialController a modelContextService

---
+++ grails-app/services/com/tim/hundreds/modelContextService.groovy
@@ -0,0 +1,42 @@
+package com.tim.hundreds
+
+class modelContextService {
+
+  def setParent(instance, params) {
+    if (params.musicianUuid) { instance.musician = Musician.findByUuid(params.musicianUuid) }
+    else if (params.companyUuid) { instance.company = Company.findByUuid(params.companyUuid) }
+    else if (params.contactUuid) { instance.contact = Contact.findByUuid(params.contactUuid) }
+    else if (params.datosFiscalesUuid) { instance.datosFiscales = Contact.findByUuid(params.datosFiscalesUuid) }
+    else {
+      log.info "INSTANCE: ${instance.dump()}"
+      log.info "PARAMS: ${params.dump()}"
+      throw new MissingParentException('Parent error')
+    }
+
+    instance
+  }
+
+  def getParamsForRedirectOnDelete(instance) {
+    if (instance.musician) {
+      request.controller = 'musician'
+      request.uuid = instance.musician.uuid
+    }
+    else if (instance.company) {
+      request.controller = 'company'
+      request.uuid = instance.company.uuid
+    }
+    else if (instance.contact) {
+      request.controller = 'contact'
+      request.uuid = instance.contact.uuid
+    }
+    else if (instance.datosFiscales) {
+      request.controller = 'datosFiscales'
+      request.uuid = instance.datosFiscales.uuid
+    }
+    else {
+      log.info "INSTANCE: ${instance.dump()}"
+      log.info "PARAMS: ${params.dump()}"
+      throw new MissingParentException('Parent error')
+    }
+  }
+}
